1955d8e1986dd9a9eb9d53cd66caf2479b6664b3
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-07-17 15:26:04 +0545

    chore: update assembly informational version to reflect latest changes

--- obj/Debug/net8.0/API.AssemblyInfo.cs
+++ obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9b534253ff4177d9cb6203e0a91339250e79480")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a08d0289be8a393470e7126468838a2d55ec51")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
