65b619bec8f723866480107cbd0b20724108957a
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-07-17 14:03:19 +0545

    feat(Controller): add api endpoints

---
+++ Controllers/StockController.cs
@@ -0,0 +1,43 @@
+using API.Data;
+using API.Mappers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+[Route("api")]
+[ApiController]
+
+public class StockController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+    public StockController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("stocks")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public IActionResult GetAll()
+    {
+        var stocks = _context.Stocks
+            .AsNoTracking()
+            .Select(x => x.ToStockDto())
+            .ToList();
+        return Ok(stocks);
+
+    }
+
+    [HttpGet("stocks/{id:int}")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult Get([FromRoute] int id)
+    {
+        var stock = _context.Stocks.SingleOrDefault(x => x.Id == id);
+        if (stock is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(stock.ToStockDto());
+    }
+}
