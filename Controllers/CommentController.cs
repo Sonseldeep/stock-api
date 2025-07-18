dff913a559e12f7b60548365dbe7542d58933eef
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-07-18 23:32:02 +0545

    feat: inject ICommentRepository into CommentController

--- Controllers/CommentController.cs
+++ Controllers/CommentController.cs
@@ -1,8 +1,13 @@
+using API.Repository.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 [ApiController]
 public class CommentController : ControllerBase
 {
-    
+    private readonly ICommentRepository _commentRepo;
+    public CommentController(ICommentRepository commentRepo)
+    {
+        _commentRepo = commentRepo;
+    }
 }
