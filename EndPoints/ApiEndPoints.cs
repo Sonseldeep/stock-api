b3a01e2950c4df7bf55095a954fcab0dcf03573d
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-07-19 23:46:16 +0545

    feat: update Create endpoint to include stockId parameter

--- EndPoints/ApiEndPoints.cs
+++ EndPoints/ApiEndPoints.cs
@@ -19,7 +19,7 @@ public static class ApiEndPoints
     {
         private const string Base = $"{ApiBase}/comments";
         public const string GetAll = Base;
-        public const string Create = Base;
+        public const string Create = $"{Base}/{{stockId:int}}";
         public const string GetById = $"{Base}/{{id:int}}";
         public const string Update = $"{Base}/{{id:int}}";
         public const string Delete = $"{Base}/{{id:int}}";
